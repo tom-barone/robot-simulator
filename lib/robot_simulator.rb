8a10afac572e929f6c32c8b4de65fd9c52dd5ce3
Tom Barone <mail@tombarone.net>
2025-07-25 22:22:21 +0930

    Move commands into correct directory

--- lib/robot_simulator.rb
+++ lib/robot_simulator.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
+require_relative 'robot_simulator/board'
+require_relative 'robot_simulator/direction'
 require_relative 'robot_simulator/errors'
 require_relative 'robot_simulator/position'
-require_relative 'robot_simulator/direction'
-require_relative 'robot_simulator/board'
 require_relative 'robot_simulator/robot'
 require_relative 'robot_simulator/robot_controller'
 require_relative 'robot_simulator/simulator'
-require_relative 'commands/command'
-require_relative 'commands/move_command'
-require_relative 'commands/left_command'
-require_relative 'commands/right_command'
 
-# Robot simulator for controlling a robot on a 5x5 table
+require_relative 'robot_simulator/commands/command'
+require_relative 'robot_simulator/commands/left_command'
+require_relative 'robot_simulator/commands/move_command'
+require_relative 'robot_simulator/commands/right_command'
+
+# Simulation of a robot moving around a board
 module RobotSimulator; end
