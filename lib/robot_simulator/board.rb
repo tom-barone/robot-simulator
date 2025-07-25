fcca93a1d174ceca4554789b9ba837566aa545b7
Tom Barone <mail@tombarone.net>
2025-07-25 20:26:48 +0930

    Add valid? method for board position

--- lib/robot_simulator/board.rb
+++ lib/robot_simulator/board.rb
@@ -9,5 +9,10 @@ module RobotSimulator
       @width = width
       @height = height
     end
+
+    def valid?(position)
+      position.x >= 0 && position.x < @width &&
+        position.y >= 0 && position.y < @height
+    end
   end
 end
--- test/unit/test_board.rb
+++ test/unit/test_board.rb
@@ -15,4 +15,52 @@ class BoardTest < Minitest::Test
     assert_equal board.width, width
     assert_equal board.height, height
   end
+
+  def test_position_0_0_is_valid_on_5x5_board
+    # Arrange
+    board = RobotSimulator::Board.new(5, 5)
+    position = RobotSimulator::Position.new(0, 0)
+
+    # Act
+    result = board.valid?(position)
+
+    # Assert
+    assert result
+  end
+
+  def test_position_4_4_is_valid_on_5x5_board
+    # Arrange
+    board = RobotSimulator::Board.new(5, 5)
+    position = RobotSimulator::Position.new(4, 4)
+
+    # Act
+    result = board.valid?(position)
+
+    # Assert
+    assert result
+  end
+
+  def test_position_negative_1_0_is_invalid
+    # Arrange
+    board = RobotSimulator::Board.new(5, 5)
+    position = RobotSimulator::Position.new(-1, 0)
+
+    # Act
+    result = board.valid?(position)
+
+    # Assert
+    refute result
+  end
+
+  def test_position_5_0_is_invalid
+    # Arrange
+    board = RobotSimulator::Board.new(5, 5)
+    position = RobotSimulator::Position.new(5, 0)
+
+    # Act
+    result = board.valid?(position)
+
+    # Assert
+    refute result
+  end
 end
